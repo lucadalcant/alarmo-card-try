597bc02b158f23750ce6e203072f4583e03ff585
lucadalcant <luca.dalcant@gmail.com>
2025-09-02 01:19:56 +0200

    Cleanup

--- src/lib/compute-domain.ts
+++
@@ -1,3 +0,0 @@
-export function computeDomain(entityId: string): string {
-  return entityId.substring(0, entityId.indexOf("."));
-}
--- src/lib/compute-entity.ts
+++
@@ -1,3 +0,0 @@
-export function computeEntity(entityId: string): string {
-  return entityId.substring(entityId.indexOf(".") + 1);
-}
